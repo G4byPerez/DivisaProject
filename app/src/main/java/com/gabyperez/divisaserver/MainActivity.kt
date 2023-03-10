2444da162ff6ce7b4a7608748faf4708ee200d41
G4byPerez <78813229+G4byPerez@users.noreply.github.com>
2023-03-09 20:09:52 -0600

    Cambios BD y Workmanager

--- app/src/main/java/com/gabyperez/divisaserver/MainActivity.kt
+++ app/src/main/java/com/gabyperez/divisaserver/MainActivity.kt
@@ -4,23 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.work.*
-import com.gabyperez.divisaserver.api.API
-import com.gabyperez.divisaserver.api.Posts
-import com.gabyperez.divisaserver.database.Moneda
 import com.gabyperez.divisaserver.database.MonedaDB
 import com.gabyperez.divisaserver.others.MyWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.count
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
-    lateinit var txt : TextView
+    private lateinit var txt : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         workManager.enqueue(workRequest)
 
         val applicationScope = CoroutineScope(SupervisorJob())
-        var lista = MonedaDB.getDatabase(applicationContext, applicationScope).MonedaDAO().getAllLista()
+        val lista = MonedaDB.getDatabase(applicationContext, applicationScope).MonedaDAO().getAllLista()
 
         for(list in lista){
             txt.append(list.code + " -> " + list.value + "\n")
